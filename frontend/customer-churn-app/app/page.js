e8ba743b8d66cf5213587e63683d5762ca973c99
Jack Torrealba <jmarcus1165@gmail.com>
2025-02-21 20:54:30 -0500

    updated form ui structure and data capture

--- frontend/customer-churn-app/app/page.js
+++ frontend/customer-churn-app/app/page.js
@@ -1,171 +1,187 @@
 'use client'
+import { formatDynamicAPIAccesses } from "next/dist/server/app-render/dynamic-rendering";
 import React, { useState } from "react";
 
-
 export default function Home() {
-  
-  const [isChecked, setIsChecked] = useState({
-    features : []
-  });
 
-  const handleCheckboxChange = (e) => {
-    const {value, checked} = e.target;
-    const {features} = isChecked;
-    console.log(e.target.value)
-    console.log(`${value} is ${checked}`)
-
-    if (checked) {
-      setIsChecked({
-        features: [...features, value]
-      });
-    };
-
-
-  }
-
+  // define form elements and their initial state
   const [formData, setFormData] = useState({
-    PhoneService: '',
-    Tenure: '',
-    MultipleLines: '',
-    OnlineSecurity: '',
-    OnlineBackup: '',
-    DeviceProtection: '',
-    TechSupport: '',
-    StreamingTV: '',
-    StreamingMovies: '',
-    PaperlessBilling: '',
-    MonthlyCharges: '',
-    TotalCharges: '',
-    InternetService: '',
-    Contract: '',
-    PaymentMethod: ''
+    PhoneService: false,
+    Tenure: 0,
+    MultipleLines: false,
+    OnlineSecurity: false,
+    OnlineBackup: false,
+    DeviceProtection: false,
+    TechSupport: false,
+    StreamingTV: false,
+    StreamingMovies: false,
+    PaperlessBilling: false,
+    MonthlyCharges: 0.00,
+    TotalCharges: 0.00,
+    InternetService: "",
+    Contract: "",
+    PaymentMethod: ""
   });
 
-  const handleChange = (event) => {
-    const {name, value } = event.target
+  const handleChange = (e) => {
+    // extract the name, type, value and checked from the target that the event fired from
+    const { name, type, value, checked } = e.target;
+    
+    // update the form's changed elements
+    setFormData((formData) => ({
+      ...formData,
+      // if the type is a checkbox then store the checked state, otherwise store the value
+      [name]: type === "checkbox" ? checked : value,
+    }));
+
   }
+
+  // function for handling form submission
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(formData)
+  }
+
+
   return (
     <div className="main">
-      <div className="form-card">
-        <form className="form">
-
-          <div className="boolean-options">
-            <div className="form-item">
-              <label htmlFor="PhoneService" name="PhoneService" value={formData.PhoneService} onChange={handleChange}>
-                <input className="form-input" type="checkbox" name="features" value="PhoneService" onChange={handleCheckboxChange}></input>
-                Phone Service: 
-              </label>
-            </div>
-            <div className="form-item">
-              <label htmlFor="MultipleLines" name="MultipleLines" value={formData.MultipleLines} onChange={handleChange}>
-                <input className="form-input" type="checkbox" name="features" value="MultipleLines" onChange={handleCheckboxChange}></input>
-                Multiple Lines:
-              </label>
-            </div>
-            <div className="form-item">
-              <label htmlFor="OnlineSecurity" name="OnlineSecurity" value={formData.OnlineSecurity} onChange={handleChange}>
-                <input className="form-input" type="checkbox" name="features" value="OnlineSecurity" onChange={handleCheckboxChange}></input>
-                Online Security: 
-              </label>
-            </div>
-            <div className="form-item">
-              <label htmlFor="OnlineBackup" name="OnlineBackup" value={formData.OnlineBackup} onChange={handleChange}>
-                <input className="form-input" type="checkbox" name="features" value="OnlineBackup" onChange={handleCheckboxChange}></input>
-                Online Backup: 
-              </label>
-            </div>
-            <div className="form-item">
-              <label htmlFor="DeviceProtection" name="DeviceProtection" value={formData.DeviceProtection} onChange={handleChange}>
-                <input className="form-input" type="checkbox" name="features" value="DeviceProtection" onChange={handleCheckboxChange}></input>
-                Device Protection: 
-              </label>
-            </div>
-            <div className="form-item">
-              <label htmlFor="TechSupport" name="TechSupport" value={formData.TechSupport} onChange={handleChange}>
-                <input className="form-input" type="checkbox" name="features" value="TechSupport" onChange={handleCheckboxChange}></input>
-                Tech Support: 
-              </label>
-            </div>
-            <div className="form-item">
-              <label htmlFor="StreamingTV" name="StreamingTV" value={formData.StreamingTV} onChange={handleChange}>
-                <input className="form-input" type="checkbox" name="features" value="StreamingTV" onChange={handleCheckboxChange}></input>
-                Streaming TV: 
-              </label>
-            </div>
-            <div className="form-item">
-              <label htmlFor="StreamingMovies" name="StreamingMovies" value={formData.StreamingMovies} onChange={handleChange}>
-                <input className="form-input" type="checkbox" name="features" value="StreamingMovies" onChange={handleCheckboxChange}></input>
-                Streaming Movies: 
-              </label>
-            </div>
-            <div className="form-item">
-              <label htmlFor="PaperlessBilling" name="PaperlessBilling" value={formData.PaperlessBilling} onChange={handleChange}>
-                <input className="form-input" type="checkbox" name="features" value="PaperlessBilling" onChange={handleCheckboxChange}></input>
-                PaperlessBilling: 
-              </label>
-            </div>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-card">
+          <div className="form-card-title">
+            <h3>Telecomm Customer Churn Predictor</h3>
           </div>
-
-          <div className="numeric-options">
-            <div className="form-item">
-              <label htmlFor="Tenure" name="Tenure" value={formData.Tenure} onChange={handleChange}>
-                Customer Tenure: 
-                <input className="form-input-num" type="number" name="Tenure"></input>
-              </label>
-            </div>
-            <div className="form-item">
-              <label htmlFor="MonthlyCharges" name="MonthlyCharges" value={formData.MonthlyCharges} onChange={handleChange}>
-                Monthly Charges: 
-                <input className="form-input-num" type="number" name="MonthlyCharges"></input>
-              </label>
-            </div>
-            <div className="form-item">
-              <label htmlFor="TotalCharges" name="TotalCharges" value={formData.TotalCharges} onChange={handleChange}>
-                Total Charges: 
-                <input className="form-input-num" type="number" name="TotalCharges"></input>
-              </label>
-            </div>
+          <div className="divider">
+            <p>Fill out the form with a customer's profile to predict whether or not the customer will churn</p>
           </div>
+          <div className="divider">
+            <hr></hr>
+          </div>
+          <div className="form-card-body">
+            <div className="boolean-options">
+              <div className="form-item">
+                <label htmlFor="PhoneService">
+                  <input className="form-input" type="checkbox" name="PhoneService" value={formData.PhoneService} onChange={handleChange}></input>
+                  Phone Service: 
+                </label>
+              </div>
+              <div className="form-item">
+                <label htmlFor="MultipleLines">
+                  <input className="form-input" type="checkbox" name="MultipleLines" value={formData.MultipleLines} onChange={handleChange}></input>
+                  Multiple Lines:
+                </label>
+              </div>
+              <div className="form-item">
+                <label htmlFor="OnlineSecurity">
+                  <input className="form-input" type="checkbox" name="OnlineSecurity" value={formData.OnlineSecurity} onChange={handleChange}></input>
+                  Online Security: 
+                </label>
+              </div>
+              <div className="form-item">
+                <label htmlFor="OnlineBackup">
+                  <input className="form-input" type="checkbox" name="OnlineBackup" value={formData.OnlineBackup} onChange={handleChange}></input>
+                  Online Backup: 
+                </label>
+              </div>
+              <div className="form-item">
+                <label htmlFor="DeviceProtection">
+                  <input className="form-input" type="checkbox" name="DeviceProtection" value={formData.DeviceProtection} onChange={handleChange}></input>
+                  Device Protection: 
+                </label>
+              </div>
+              <div className="form-item">
+                <label htmlFor="TechSupport">
+                  <input className="form-input" type="checkbox" name="TechSupport" value={formData.TechSupport} onChange={handleChange}></input>
+                  Tech Support: 
+                </label>
+              </div>
+              <div className="form-item">
+                <label htmlFor="StreamingTV">
+                  <input className="form-input" type="checkbox" name="StreamingTV" value={formData.StreamingTV} onChange={handleChange}></input>
+                  Streaming TV: 
+                </label>
+              </div>
+              <div className="form-item">
+                <label htmlFor="StreamingMovies">
+                  <input className="form-input" type="checkbox" name="StreamingMovies" value={formData.StreamingMovies} onChange={handleChange}></input>
+                  Streaming Movies: 
+                </label>
+              </div>
+              <div className="form-item">
+                <label htmlFor="PaperlessBilling">
+                  <input className="form-input" type="checkbox" name="PaperlessBilling" value={formData.PaperlessBilling} onChange={handleChange}></input>
+                  PaperlessBilling: 
+                </label>
+              </div>
+            </div>
 
-          <div className="dropdown-options">
-            <div className="form-item">
-              <label htmlFor="InternetService" name="InternetService" value={formData.InternetService} onChange={handleChange}>
-                Internet Type: 
-                <select className="form-input-dropdown">
-                  <option selected disabled></option>
+            <div className="numeric-options">
+              <div className="form-item-num">
+                <label htmlFor="Tenure">
+                  Customer Tenure: 
+                </label>
+                <input className="form-input-num" type="number" name="Tenure"  value={formData.Tenure} onChange={handleChange}></input>
+              </div>
+              <div className="form-item-num">
+                <label htmlFor="MonthlyCharges">
+                  Monthly Charges: 
+                </label>
+                <input className="form-input-num currency" step="0.01" type="number" name="MonthlyCharges" value={formData.MonthlyCharges} onChange={handleChange}></input>
+              </div>
+              <div className="form-item-num">
+                <label htmlFor="TotalCharges">
+                  Total Charges: 
+                </label>
+                <input className="form-input-num currency" step="0.01" type="number" name="TotalCharges" value={formData.TotalCharges} onChange={handleChange}></input>
+              </div>
+            </div>
+
+            <div className="dropdown-options">
+              <div className="form-item-select">
+                <label htmlFor="InternetService">
+                  Internet Type: 
+                </label>
+                <select className="form-input-dropdown" onChange={handleChange} defaultValue={formData.InternetService}>
+                  <option value="" disabled></option>
                   <option value="dsl">DSL</option>
                   <option value="Fiberoptic">Fiber Optic</option>
                   <option value="no">None</option>
                 </select>
-              </label>
-            </div>
-            <div className="form-item">
-              <label htmlFor="Contract" name="Contract" value={formData.Contract} onChange={handleChange}>
-                Contract Type: 
-                <select className="form-input-dropdown">
-                  <option selected disabled></option>
+              </div>
+              <div className="form-item-select">
+                <label htmlFor="Contract" >
+                  Contract Type: 
+                </label>
+                <select className="form-input-dropdown" onChange={handleChange} defaultValue={formData.Contract}>
+                  <option value="" disabled></option>
                   <option value="month-to-month">Month-to-Month</option>
                   <option value="one year">1 year</option>
                   <option value="two year">2 Year</option>
                 </select>
-              </label>
-            </div>
-            <div className="form-item">
-              <label htmlFor="PaymentMethod" name="PaymentMethod" value={formData.PaymentMethod} onChange={handleChange}>
-                Payment Method: 
-                <select className="form-input-dropdown">
-                  <option selected disabled></option>
+              </div>
+              <div className="form-item-select">
+                <label htmlFor="PaymentMethod">
+                  Payment Method: 
+                </label>
+                <select className="form-input-dropdown" onChange={handleChange} defaultValue={formData.PaymentMethod}>
+                  <option value="" disabled></option>
                   <option value="bank transfer">Bank Transfer</option>
                   <option value="credit card">Credit Card</option>
                   <option value="electronic check">E-Check</option>
                   <option value="mailed check">Mailed Check</option>
                 </select>
-              </label>
+              </div>
             </div>
           </div>
-
-        </form>
-      </div>
+          <div className="divider">
+            <hr></hr>
+          </div>
+          <div className="form-card-footer">
+            <div className="predict-div">
+              <button id="predict-btn" type="submit">Predict</button>
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
