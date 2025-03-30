8ef8c3d38609e0d37faf546a5ac472f3a6519d53
Jack Torrealba <jmarcus1165@gmail.com>
2025-03-30 17:30:02 -0400

    modified api url

--- frontend/customer-churn-app/app/page.js
+++ frontend/customer-churn-app/app/page.js
@@ -87,7 +87,7 @@ export default function Home() {
     };
 
     try {
-      const response = await fetch('http://localhost:5000/predict', {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/predict`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
